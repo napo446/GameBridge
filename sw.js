f66489b3afd048dda336edbfd0f94f00d4ed6800
napo446 <napotshepo9@gmail.com>
2025-12-27 08:29:34 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,74 @@
+Microsoft Windows [Version 10.0.26200.7171]
+(c) Microsoft Corporation. All rights reserved.
 
+C:\Users\tshep\GameBridge>npm install -g firebase-tools
+npm warn deprecated node-domexception@1.0.0: Use your platform's native DOMException instead
+
+added 1 package, and changed 749 packages in 5m
+
+92 packages are looking for funding
+  run `npm fund` for details
+
+C:\Users\tshep\GameBridge>firebase login
+Already logged in as tsheponapo44@gmail.com
+
+C:\Users\tshep\GameBridge>firebase init
+
+     ######## #### ########  ######## ########     ###     ######  ########
+     ##        ##  ##     ## ##       ##     ##  ##   ##  ##       ##
+     ######    ##  ########  ######   ########  #########  ######  ######
+     ##        ##  ##    ##  ##       ##     ## ##     ##       ## ##
+     ##       #### ##     ## ######## ########  ##     ##  ######  ########
+
+You're about to initialize a Firebase project in this directory:
+
+  C:\Users\tshep
+
+Before we get started, keep in mind:
+
+  * You are initializing within an existing Firebase project directory
+
+√ Are you ready to proceed? No
+
+Error: Aborted by user.
+
+C:\Users\tshep\GameBridge>C:Users\tshep\GameBridge
+The system cannot find the path specified.
+
+C:\Users\tshep\GameBridge>cd:C:\Users\tshep\GameBridge
+The filename, directory name, or volume label syntax is incorrect.
+
+C:\Users\tshep\GameBridge>C:\Users\tshep\GameBridge
+'C:\Users\tshep\GameBridge' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\tshep\GameBridge>firebase init
+
+     ######## #### ########  ######## ########     ###     ######  ########
+     ##        ##  ##     ## ##       ##     ##  ##   ##  ##       ##
+     ######    ##  ########  ######   ########  #########  ######  ######
+     ##        ##  ##    ##  ##       ##     ## ##     ##       ## ##
+     ##       #### ##     ## ######## ########  ##     ##  ######  ########
+
+You're about to initialize a Firebase project in this directory:
+
+  C:\Users\tshep
+
+Before we get started, keep in mind:
+
+  * You are initializing within an existing Firebase project directory
+
+√ Are you ready to proceed? Yes
+√ Which Firebase features do you want to set up for this directory? Press Space to select features, then Enter to confirm your choices. Firestore:
+Configure security rules and indexes files for Firestore, Hosting: Set up deployments for static web apps
+
+=== Project Setup
+
+First, let's associate this project directory with a Firebase project.
+You can create multiple project aliases by running firebase use --add,
+but for now we'll just set up a default project.
+
+
+Error: Failed to get Firebase project eduproassistprj. Please make sure the project exists and your account has permission to access it.
+
+C:\Users\tshep\GameBridge>
