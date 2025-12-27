6ce35af92a709e619af1f18e0a7b55310746c4c0
napo446 <napotshepo9@gmail.com>
2025-12-27 07:51:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-
+? Which Firebase features do you want to set up for this directory? Press Space to select features, then Enter to
+confirm your choices.
+ ◯ Genkit: Setup a new Genkit project with Firebase
+ ◯ Functions: Configure a Cloud Functions directory and its files
+ ◯ App Hosting: Set up deployments for full-stack web apps (supports server-side rendering)
+❯◯ Hosting: Set up deployments for static web apps
+ ◯ Storage: Configure a security rules file for Cloud Storage
+ ◯ Emulators: Set up local emulators for Firebase products
+ ◯ Remote Config: Configure a template file for Remote Config
