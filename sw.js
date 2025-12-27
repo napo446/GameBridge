d8c48cbcd023baed081fa5ff129c6d3a691445ed
napo446 <napotshepo9@gmail.com>
2025-12-27 09:54:40 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,23 @@
-PS C:\Users\tshep\GameBridge> firebase projects:list
-✔ Preparing the list of your Firebase projects
-┌──────────────────────┬──────────────────────┬────────────────┬──────────────────────┐
-│ Project Display Name │ Project ID           │ Project Number │ Resource Location ID │
-├──────────────────────┼──────────────────────┼────────────────┼──────────────────────┤
-│ GameBridge           │ gamebridge-f4789     │ 562553872482   │ [Not specified]      │
-├──────────────────────┼──────────────────────┼────────────────┼──────────────────────┤
-│ my-vite-firebase-app │ my-vite-firebase-app │ 447240447085   │ [Not specified]      │
-└──────────────────────┴──────────────────────┴────────────────┴──────────────────────┘
+Microsoft Windows [Version 10.0.26200.7171]
+(c) Microsoft Corporation. All rights reserved.
 
-2 project(s) total.
-PS C:\Users\tshep\GameBridge> 
+C:\Users\tshep\GameBridge>npm install -g firebase-tools
+npm warn deprecated node-domexception@1.0.0: Use your platform's native DOMException instead
+
+added 1 package, and changed 749 packages in 5m
+
+92 packages are looking for funding
+  run `npm fund` for details
+
+C:\Users\tshep\GameBridge>firebase login
+Already logged in as tsheponapo44@gmail.com
+
+C:\Users\tshep\GameBridge>cd C:\Users\tshep\GameBridge
+
+C:\Users\tshep\GameBridge>firebase use gamebridge-f4789 --alias default
+
+Error: firebase use must be run from a Firebase project directory.
+
+Run firebase init to start a project directory in the current folder.
+
+C:\Users\tshep\GameBridge>
